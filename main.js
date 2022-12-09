8518cc9afbc834913d885d742eddac779a081c4e
Anaz Anoiar <98679895+AnazAnoiar69@users.noreply.github.com>
2022-12-09 12:04:33 +0800

    deploy 20

--- main.js
+++ main.js
@@ -1,37 +1,62 @@
-import { showAlertIcon } from "./src/components";
-
 const form = document.getElementById("sign-up-form");
-
-const cFname = document.querySelector(".f-name");
-const cFnameInput = document.querySelector(".f-name-input");
-const cLname = document.querySelector(".l-name");
-const cLnameInput = document.querySelector(".l-name-input");
+const fnameInput = document.getElementById("f-name");
+const lnameInput = document.getElementById("l-name");
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const fnameContainer = document.querySelector(".f-name-input");
+const lnameContainer = document.querySelector(".l-name-input");
+const emailContainer = document.querySelector(".email-input");
+const passwordContainer = document.querySelector(".password-input"); 
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  validateForm();
+  validateForm()
 });
 
-function validateForm() {
-  const fname = document.getElementById("f-name");
-  // const lname = document.getElementById("l-name");
-  // const email = document.getElementById("email");
-  // const password = document.getElementById("password");
+var mailformat =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+const validateForm = () => {
+  fnameInput.value === ""
+    ? showError(fnameContainer, fnameInput, "First Name cannot be empty")
+    : showSuccess(fnameContainer, fnameInput, "");
+  lnameInput.value === ""
+    ? showError(lnameContainer, lnameInput, "Last Name cannot be empty")
+    : showSuccess(lnameContainer, lnameInput, "");
+  emailInput.value === ""
+    ? !emailInput.value.match(mailformat)
+      ? showError(emailContainer, emailInput, "Email cannot be empty")
+      : showError(emailContainer, emailInput, "Not a valid Email Address")
+    : showSuccess(emailContainer, emailInput, "");
+  passwordInput.value === ""
+    ? showError(passwordContainer, passwordInput, "Password cannot be empty")
+    : passwordInput.value.length <= 8 - 1
+    ? showError(
+        passwordContainer,
+        passwordInput,
+        "Password must contain min 8 character"
+      )
+    : showSuccess(passwordContainer, passwordInput, "");
+  
+};
+const showError = (pe, ce, message) => {
+  const span = ce.nextElementSibling;
+  ce.classList.add("error");
+  span.classList.remove("hidden");
+  const errMessage = pe.nextElementSibling;
+  errMessage.innerText = message;
+  errMessage.classList.remove("hidden");
+};
+const showSuccess = (pe, ce, message) => {
+  const span = ce.nextElementSibling;
+  ce.classList.remove("error");
+  span.classList.add("hidden");
+  const errMessage = pe.nextElementSibling;
+  errMessage.innerText = message;
+  errMessage.classList.add("hidden");
     fname.value === ""
       ? showError(fname, "First Name cannot be empty")
       : showSuccess(fname, "First Name available");
 
       
 }
-
-const showError = (element, message) => {
-  element.classList.add("border-red-700");
-  const alertIcon = showAlertIcon()
-  cFnameInput.append(alertIcon)
-  // cLnameInput.append(alertIcon)
-
-  const pop = document.createElement('p')
-  pop.classList.add("text-[.6rem]", "text-red-600", "text-end", "italic", "text-bold")
-  pop.innerHTML = message;
-};
